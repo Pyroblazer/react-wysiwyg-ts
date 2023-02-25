5c38a4440a3fbf3d3e43e79e16f0805dcea9d0aa
Pyroblazer <ignatiustimothymanullang@gmail.com>
2023-02-26 04:43:33 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react"
+import { EditorState } from "draft-js"
+import { Editor } from "react-draft-wysiwyg"
+import { convertToHTML } from "draft-convert"
+import DOMPurify from "dompurify"
+
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css"
+import "./App.css"
+
+function App() {
+    const [editorState, setEditorState] = useState(() =>
+        EditorState.createEmpty()
+    )
+    const [convertedContent, setConvertedContent] = useState("")
+
+    useEffect(() => {
+        let html = convertToHTML(editorState.getCurrentContent())
+        setConvertedContent(html)
+    }, [editorState])
+
+    function createMarkup(html: string | Node) {
+        return {
+            __html: DOMPurify.sanitize(html),
+        }
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">Rich Text Editor Example</header>
+            <Editor
+                editorState={editorState}
+                onEditorStateChange={setEditorState}
+                wrapperClassName="wrapper-class"
+                editorClassName="editor-class"
+                toolbarClassName="toolbar-class"
+                toolbar={{
+                    options: ["inline", "blockType"],
+                }}
+                hashtag={{
+                    separator: " ",
+                    trigger: "#",
+                }}
+                mention={{
+                    separator: " ",
+                    trigger: "@",
+                    suggestions: [
+                        { text: "JavaScript", value: "javascript", url: "js" },
+                        { text: "Golang", value: "golang", url: "go" },
+                    ],
+                }}
+            />
+            <div
+                className="preview"
+                dangerouslySetInnerHTML={createMarkup(convertedContent)}
+            ></div>
+        </div>
+    )
+}
+
+export default App
